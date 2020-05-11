38f8e7b2d5d3ab9c727d9b8faaf8c3fa03af4e59
Samuel Weslley M. Barboza <SWBarboza@raiadrogasil.com>
2020-05-11 19:39:56 -0300

    Spring Boot Microservices 05 - Authentication Service Signed and Encrypted JWT (JWS+JWE) pt 02

--- auth/src/main/java/academy/devdojo/youtube/auth/security/filter/JwtUsernameAndPasswordAuthenticationFilter.java
+++ auth/src/main/java/academy/devdojo/youtube/auth/security/filter/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,11 +1,120 @@
 package academy.devdojo.youtube.auth.security.filter;
 
+import academy.devdojo.youtube.core.model.ApplicationUser;
+import academy.devdojo.youtube.core.property.JwtConfiguration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.DirectEncrypter;
+import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Collections;
+import java.util.Date;
+import java.util.UUID;
+
+import static java.util.stream.Collectors.toList;
 
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Slf4j
 public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtConfiguration jwtConfiguration;
+
+    @Override
+    @SneakyThrows
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
+        ApplicationUser applicationUser = new ObjectMapper().readValue(request.getInputStream(), ApplicationUser.class);
+        if (applicationUser == null ) {
+            throw new UsernameNotFoundException("Unable to retrieve the username or password");
+        }
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(applicationUser.getUsername(), applicationUser.getPassword(), Collections.emptyList());
+        usernamePasswordAuthenticationToken.setDetails(applicationUser);
+        return authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+    }
+
+    @Override
+    @SneakyThrows
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication auth) throws IOException, ServletException {
+        SignedJWT signedJWT = createSignedJWT(auth);
+        String encryptedToken = encryptToken(signedJWT);
+
+        response.addHeader("Access-Control-Expose-Headers", "XSRF-TOKEN, "+ jwtConfiguration.getHeader().getName());
+
+        response.addHeader(jwtConfiguration.getHeader().getName(), jwtConfiguration.getHeader().getPrefix() + encryptedToken);
+    }
+
+    @SneakyThrows
+    private SignedJWT createSignedJWT(Authentication auth) {
+        ApplicationUser applicationUser = (ApplicationUser) auth.getPrincipal();
+
+        JWTClaimsSet jwtClaimsSet =  createJwtClaimsSet(auth, applicationUser);
+
+        KeyPair rsaKey = generateKeyPair();
+
+        JWK jwk = new RSAKey.Builder((RSAPublicKey) rsaKey.getPublic()).keyID(UUID.randomUUID().toString()).build();
+
+        SignedJWT signedJWT = new SignedJWT(new JWSHeader.Builder(JWSAlgorithm.RS256)
+            .jwk(jwk)
+            .type(JOSEObjectType.JWT)
+            .build(), jwtClaimsSet);
+
+        RSASSASigner signer = new RSASSASigner(rsaKey.getPrivate());
+
+        signedJWT.sign(signer);
+
+        return signedJWT;
+    }
+
+    private JWTClaimsSet createJwtClaimsSet(Authentication auth, ApplicationUser applicationUser) {
+        return new JWTClaimsSet.Builder()
+                .subject(applicationUser.getUsername())
+                .claim("authorities", auth.getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(toList()))
+                        .issuer("http://academy.devdojo")
+                        .issueTime(new Date())
+                        .expirationTime(new Date(System.currentTimeMillis() + (jwtConfiguration.getExpiration() * 1000)))
+                .build();
+    }
+
+    @SneakyThrows
+    private KeyPair generateKeyPair() {
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        generator.initialize(2048);
+        return generator.genKeyPair();
+    }
+
+    private String encryptToken(SignedJWT signedJWT) throws JOSEException {
+        DirectEncrypter directEncrypter = new DirectEncrypter(jwtConfiguration.getPrivateKey().getBytes());
+        JWEObject jweObject = new JWEObject(new JWEHeader.Builder(JWEAlgorithm.DIR, EncryptionMethod.A128CBC_HS256)
+                .contentType("JWT")
+                .build(), new Payload(signedJWT));
+        jweObject.encrypt(directEncrypter);
+
+        return jweObject.serialize();
+    }
 }
