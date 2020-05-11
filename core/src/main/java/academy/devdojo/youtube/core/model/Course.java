cc4e60809cac1e39e1e20d61908a6bfe9806d702
Samuel Weslley M. Barboza <SWBarboza@raiadrogasil.com>
2020-05-11 15:04:40 -0300

    subindo para o github

---
+++ core/src/main/java/academy/devdojo/youtube/core/model/Course.java
@@ -0,0 +1,25 @@
+package academy.devdojo.youtube.core.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Course implements AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NotNull(message = "The field 'title' is mandatory")
+    @Column(name = "DS_TITLE", nullable = false)
+    private String title;
+}
